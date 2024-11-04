bf1fb7894c08869976fe9384f4d50aa82aa7f4f6
kvroegop <k.vroegop@first8.nl>
2024-11-04 12:53:20 +0100

    initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO audiobook (isbn, title, author, reader, publish_date, playtime) VALUES
+  ('9780446310789', 'To Kill a Mockingbird', 'Harper Lee', 'Nigel Planer', '1960-07-11', '737'),
+  ('9780743273565', 'The Great Gatsby', 'F. Scott Fitzgerald Linda Cookson',  'Nigel Planer','1925-04-10', '289'),
+  ('9780006546061', 'Fahrenheit 451', 'Fahrenheit 451', 'Nigel Planer','1953-10-13', '301' ),
+  ('9780140283334', 'Lord of the Flies', 'William Golding', 'Nigel Planer','1954-09-17', '395'),
+  ('9780142000670', 'Of Mice and Men', 'John Steinbeck', 'Nigel Planer','1937-02-25', '191'),
+  ('9780060929879', 'Brave New World', 'Brave New World', 'Nigel Planer','1932-09-01', '480'),
+  ('9780316769174', 'The Catcher in the Rye', 'J.D. Salinger', 'Nigel Planer','1951-07-16', '191'),
+  ('9780141036144', '1984', 'George Orwell', 'Nigel Planer','1949-04-08', '798'),
+  ('9780393320978', 'Beowulf', 'Unknown', 'Nigel Planer','900-01-01', '133'),
+  ('9780060502935', 'Going Postal', 'Terry Pratchett', 'Nigel Planer','2004-09-25', '683');
+
